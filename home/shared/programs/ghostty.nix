9d85f5378ea2baf9d76bc921cd7592c9868c4edf
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-01-16 18:08:12 +0700

    home(shared[ghostty]): modify ghostty config

--- home/shared/programs/ghostty.nix
+++ home/shared/programs/ghostty.nix
@@ -1,29 +1,29 @@
 { config, ... }:
 {
-    programs.ghostty = {
-        enable = true;
-        enableFishIntegration = true;
-        clearDefaultKeybinds = false;
-        installBatSyntax = true;
-        settings = {
-            adjust-cell-height = "15%";
-            background-opacity = 1;
-            cursor-style = "underline";
-            cursor-style-blink = true;
-            font-family = "JetBrains Mono";
-            font-feature = "+calt, +liga, +dlig";
-            font-size = 11;
-            link-previews = true;
-            shell-integration = if config.programs.fish.enable then "fish" else "detect";
-            shell-integration-features = "no-cursor";
-            theme = "dark:TokyoNight,light:TokyoNight Day";
-            window-decoration = "auto";
-            window-padding-balance = true;
-            window-padding-x = 3;
-            window-padding-y = 3;
-            window-show-tab-bar = "never";
-            window-theme = "system";
-        };
-        systemd.enable = true;
+  programs.ghostty = {
+    enable = true;
+    enableFishIntegration = true;
+    clearDefaultKeybinds = false;
+    installBatSyntax = true;
+    settings = {
+      adjust-cell-height = "15%";
+      background-opacity = 1;
+      cursor-style = "underline";
+      cursor-style-blink = true;
+      font-family = "JetBrains Mono";
+      font-feature = "+calt, +liga, +dlig";
+      font-size = 11;
+      link-previews = true;
+      shell-integration = if config.programs.fish.enable then "fish" else "detect";
+      shell-integration-features = "no-cursor";
+      theme = "dark:TokyoNight,light:TokyoNight Day";
+      window-decoration = "auto";
+      window-padding-balance = true;
+      window-padding-x = 3;
+      window-padding-y = 3;
+      window-show-tab-bar = "never";
+      window-theme = "system";
     };
+    systemd.enable = true;
+  };
 }

9bcb4c1c27088f90ee20548f92737dd11ef5e403
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-01-27 19:19:36 +0700

    nixos: update flake
    Signed-off-by: Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -1,34 +1,28 @@
 { config
+, lib
+, pkgs
 , modulesPath
 , ...
 }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-
-  boot.initrd.luks.devices = {
-    "luks-37355eb9-fb07-4aa6-8a00-71b84622d91f".device =
-      "/dev/disk/by-uuid/37355eb9-fb07-4aa6-8a00-71b84622d91f";
-    "luks-1ec17656-96de-4a40-9b68-d365f3bcf466".device =
-      "/dev/disk/by-uuid/1ec17656-96de-4a40-9b68-d365f3bcf466";
-  };
-
-  fileSystems."/" = {
-    device = "/dev/mapper/luks-37355eb9-fb07-4aa6-8a00-71b84622d91f";
-    fsType = "ext4";
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/mapper/luks-1ec17656-96de-4a40-9b68-d365f3bcf466";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/ESP_SYSTEM";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
+  imports =
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
-  };
+
+  boot.initrd.luks.devices."luks-4f63db25-9e60-4a4d-9726-9dcd03c32d39".device = "/dev/disk/by-uuid/4f63db25-9e60-4a4d-9726-9dcd03c32d39";
+
+  fileSystems."/" =
+    {
+      device = "/dev/mapper/luks-4f63db25-9e60-4a4d-9726-9dcd03c32d39";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/5BD4-AFB5";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 }
--- users/shared/default.nix
+++ users/shared/default.nix
@@ -31,9 +31,130 @@
       enable = true;
       interactiveShellInit = ''
         set fish_greeting
-        function fish_right_prompt
-          date +"%H:%M:%S"
+
+        function commit
+          if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
+            echo "[!] Not in a Git repository."
+            return 1
+          end
+
+          set cmsg $argv[1]
+          set mode $argv[2]
+          set file $argv[3..-1]
+
+          if test -z "$cmsg"
+            echo "[!] Commit messages cannot be empty"
+            return 1
+          end
+
+          test -n "$mode"; or set mode all
+
+          echo "[1/3] Adding change(s) to the staging area..."
+          switch $mode
+            case all
+              git add -A
+              or begin
+                echo "[!] Failed at step 1/3"
+                return 1
+              end
+            case file
+              if test (count $files) -eq 0
+                  echo "[!] No files specified for mode 'file'"
+                  return 1
+              end
+              git add $file
+              or begin
+                echo "[!] Failed at step 1/3"
+                return 1
+              end
+            case '*'
+              echo "Invalid mode: $mode"
+              return 1
+          echo "[1/3] Done."
+
+          echo "[2/3] Committing staged changes..."
+          git commit -s -m "$cmsg"
+          or begin
+            echo "[!] Failed at step 2/3"
+            return 1
+          end
+          echo "[2/3] Done."
+
+          echo "[3/3] Pushing commits..."
+          git push
+          or begin
+            echo "[!] Failed at step 3/3"
+            return 1
+          end
+          echo "[3/3] Done."
         end
+
+        function update
+            set _cdir (pwd)
+
+            echo "[1/7] Change directory to flake repository..."
+            cd /home/plucky/Projects/nixos
+            or begin
+              echo "[!] Failed at step 1/7"
+              return 1
+            end
+            echo "[1/7] Done."
+
+            echo "[2/7] Updating flakes..."
+            nix flake update
+            or begin
+              echo "[!] Failed at step 2/7"
+              cd $_cdir
+              return 1
+            end
+            echo "[2/7] Done."
+
+            echo "[3/7] Formatting repository..."
+            nix fmt
+            or begin
+              echo "[!] Failed at step 3/7"
+              cd $_cdir
+              return 1
+            end
+            echo "[3/7] Done."
+
+            echo "[4/7] Pushing changes to remote repository..."
+            commit "nixos: update flake" all
+            or begin
+              echo "[!] Failed at step 4/7"
+              cd $_cdir
+              return 1
+            end
+            echo "[4/7] Done."
+
+            echo "[5/7] Rebuilding host system..."
+            sudo nixos-rebuild switch --flake .#puffin
+            or begin
+              echo "[!] Failed at step 5/7"
+              cd $_cdir
+              return 1
+            end
+            echo "[5/7] Done."
+
+            echo "[6/7] Deleting older generations..."
+            sudo nix-collect-garbage -d
+            or begin
+              echo "[!] Failed at step 6/7"
+              cd $_cdir
+              return 1
+            end
+            echo "[6/7] Done."
+
+            echo "[7/7] Back to last directory..."
+            cd $_cdir
+            or begin
+              echo "[!] Failed at step 7/7"
+              cd $_cdir
+              return 1
+            end
+            echo "[7/7] Done."
+        end
+
         zoxide init fish | source
       '';
       preferAbbrs = true;
@@ -43,7 +164,10 @@
         cd = "z";
         l = "eza -lahgmuU --smart-group";
         ls = "eza";
-        ncg = "sudo nix-collect-garbage -d";
+        ngc = "sudo nix-collect-garbage -d";
+        nfu = "nix flake update";
+        nft = "nix fmt";
+        nrd = "sudo nixos-rebuild dry-run --flake";
         nrs = "sudo nixos-rebuild switch --flake";
         x = "exit";
       };

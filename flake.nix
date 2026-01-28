b96d7f21fbc1e7bb5e8c3c0423721018c493bbfb
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-01-28 12:50:54 +0700

    nixos: change nixpkgs branch from master -> nixos-unstable
    Signed-off-by: Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     self.submodules = true;
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";

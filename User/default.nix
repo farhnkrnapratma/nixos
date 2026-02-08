5a946cccbc3dbb540c425c23c94c5eab9860f3ad
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-02-08 22:08:21 +0700

    user(xdg.dataFile): fix forbidden access to absolute path '/home' in `xdg.dataFile.icons.source`
    Signed-off-by: Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>

--- User/default.nix
+++ User/default.nix
@@ -580,7 +580,7 @@ in
       enable = true;
       ignorelinks = true;
       recursive = true;
-      source = "${my.path.flake}/User/Files/Icons";
+      source = "./Files/Icons";
       target = "${config.xdg.dataHome}/icons";
     };
     desktopEntries =
